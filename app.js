644b7a0c30f17e908bfdce4d578dea2699ea43ba
lucas.chinarelli <lucas.chinarelli@rpinfo.com.br>
2020-02-23 00:15:36 -0300

    Criação de rotas testes de USUARIOS

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 const models = require('./app/models/');
 const Usuario = models.Usuario;
 
-Usuario.create({ nome: 'Lucas', sobrenome: 'Chinarelli', email: 'lucas@humobu.com.br', senha: '123456',
-                 celular: '0101010101', dtnascimento: '2020-02-21', sexo: 'M',
-                 rg: '91919191', dtemissao: '2020-02-21', orgaoemissor: 'SSP', cpf: '1173737373',
-                 estadocivil: 'S', profissao: 'Desenvolvedore de Software', empresa: 'Sponte' }); 
-
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/usuarios', (req, res) => {  //Listar todos
+
+}); 
+
+app.post('/usuarios', async (req, res) => { // Criar
+  const usuario = await Usuario.create(req.body);
+  res.json('Cliente Cadastrado Com Sucesso');
+});
+
+app.get('/usuarios/:id', (req, res) => { //Buscar
+
+}); 
+
+app.put('/usuarios/:id', (req, res) => { //Editar
+
+}); 
+
+app.delete('/usuarios/:id', async (req, res) => { //Deletar
+  const usuario = await Usuario.delete(req.body);
+  res.json('Cliente Deletado Com Sucesso');
+}); 
 
 app.listen(3000);
